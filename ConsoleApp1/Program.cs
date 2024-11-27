625ffde6b544731459855d93b880a3eaf296f7ff
SudhaPuchala <sudhavardhann@gmail.com>
2024-11-28 00:25:41 +0100

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            decimal b = 3m;
+            decimal sum;
+
+            sum = a + b;
+            Console.WriteLine($"sum of {a} and {b} is {sum}");
+
+           
+        }
+    }
+}
